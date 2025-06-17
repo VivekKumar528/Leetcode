5c510d13ef5f5656e4c23b4d539eb7e2c7acd4c3
VivekKumar528 <122791268+VivekKumar528@users.noreply.github.com>
2025-06-17 09:12:02 +0530

    Time: 6 ms (44.73%) | Memory: 45.2 MB (7.20%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.java
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -1,17 +1,28 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-        int countU = 0;
-        int countD = 0;
-        int countL = 0;
-        int countR = 0;
+        // int countU = 0;
+        // int countD = 0;
+        // int countL = 0;
+        // int countR = 0;
+        // for(int i=0;i<moves.length();i++){
+        //     char ch = moves.charAt(i);
+        //     if(ch == 'U') countU++;
+        //     else if(ch == 'D') countD++;
+        //     else if(ch == 'L') countL++;
+        //     else countR++;
+        // }
+        // if(countU == countD && countL == countR) return true;
+        // return false;
+        int x = 0;
+        int y = 0;
         for(int i=0;i<moves.length();i++){
             char ch = moves.charAt(i);
-            if(ch == 'U') countU++;
-            else if(ch == 'D') countD++;
-            else if(ch == 'L') countL++;
-            else countR++;
+            if(ch == 'U') x--;
+            else if(ch == 'D') x++;
+            else if(ch == 'L') y--;
+            else y++;
         }
-        if(countU == countD && countL == countR) return true;
-        return false;
+        return x == 0 && y == 0;
+        
     }
 }
