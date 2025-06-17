38a43ca72187ce950d95587a7a3531fd3ba9affa
VivekKumar528 <122791268+VivekKumar528@users.noreply.github.com>
2025-06-17 08:45:12 +0530

    Time: 10 ms (6.00%) | Memory: 45.9 MB (94.21%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,4 +1,21 @@
 class Solution {
+    public void BFS(Map<Integer, List<Integer>> map, int u, boolean[] vis){
+        vis[u] = true;
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(u);
+        while(q.size() > 0){
+            int n = q.size();
+            for(int i=0;i<n;i++){
+                int ele = q.poll();
+                for(int e : map.get(ele)){
+                    if(!vis[e]) {
+                        q.offer(e);
+                        vis[e] = true;
+                    }
+                }
+            }
+        }
+    }
     public void DFS(Map<Integer, List<Integer>> map, int u, boolean[] vis){
         vis[u] = true;
         for(int v : map.get(u)){
@@ -23,7 +40,8 @@ class Solution {
         int count = 0;
         for(int i=1;i<=n;i++){
             if(!vis[i]) {
-                DFS(map, i, vis);
+                // DFS(map, i, vis);
+                BFS(map, i, vis);
                 count++;
                 
             }
