9afedc0079b5aa9fb9a4730fddbc087d30c230ba
VivekKumar528 <122791268+VivekKumar528@users.noreply.github.com>
2025-07-14 16:52:51 +0530

    Time: 2 ms (99.65%) | Memory: 43.3 MB (70.47%) - LeetSync

--- 1547-destination-city/destination-city.java
+++ 1547-destination-city/destination-city.java
@@ -1,20 +1,18 @@
 class Solution {
     public String destCity(List<List<String>> list) {
         HashMap<String, Integer> map = new HashMap<>();
-        for(int i=0;i<list.size();i++){
-            for(int j=0;j<list.get(0).size();j++){
-                String key = list.get(i).get(0);
-                if(map.containsKey(key)){
-                    int freq = map.get(key);
-                    map.put(key, freq+1);
-                } else map.put(key, 1);
-            }
+        for (int i = 0; i < list.size(); i++) {
+            String key = list.get(i).get(0);
+            if (map.containsKey(key)) {
+                int freq = map.get(key);
+                map.put(key, freq + 1);
+            } else
+                map.put(key, 1);
         }
-        for(int i=0;i<list.size();i++){
-            for(int j=0;j<list.get(0).size();j++){
-                String key = list.get(i).get(1);
-                if(!map.containsKey(key) || map.get(key) == 0) return key;
-            }
+        for (int i = 0; i < list.size(); i++) {
+            String key = list.get(i).get(1);
+            if (!map.containsKey(key) || map.get(key) == 0)
+                return key;
         }
         return "";
 
