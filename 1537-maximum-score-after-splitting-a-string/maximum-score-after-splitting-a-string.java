ce7debf3d7e77f3d680dbfee0a86774666f70d22
VivekKumar528 <122791268+VivekKumar528@users.noreply.github.com>
2025-07-16 09:47:27 +0530

    Time: 1 ms (99.16%) | Memory: 41.6 MB (48.04%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
@@ -1,29 +1,18 @@
 class Solution {
-    public int countZero(int i, int j, String str){
-        int count = 0;
-        for(int x=i;x<=j;x++){
-            char ch = str.charAt(x);
-            if(ch == '0') count++;
-        }
-        return count;
-    }
-    public int countOnes(int i, int j, String str){
-        int count = 0;
-        for(int x=i;x<=j;x++){
-            char ch = str.charAt(x);
-            if(ch == '1') count++;
-        }
-        return count;
-    }
     public int maxScore(String str) {
-        int zero = 0;
-        int ones = 0;
-        int max = 0;
-        for(int i=0;i<=str.length()-2;i++){
-            zero = countZero(0, i, str);
-            ones = countOnes(i+1, str.length()-1, str);
-            max = Math.max(max, zero + ones);
+        int totalOnes = 0;
+        int res = 0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) == '1') totalOnes++;
         }
-        return max;
+        int zero = 0;
+        int one = 0;
+        for(int i=0;i<=str.length()-2;i++){
+            if(str.charAt(i) == '0') zero++;
+            else one++;
+            int rightOnes = totalOnes - one;
+            res = Math.max(res, zero + rightOnes);
+        }
+        return res;
     }
 }
