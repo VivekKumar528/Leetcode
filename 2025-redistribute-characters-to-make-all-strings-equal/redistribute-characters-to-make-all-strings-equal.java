d50df706e75f3ad6588de2e562d470d4d9d338b8
VivekKumar528 <122791268+VivekKumar528@users.noreply.github.com>
2025-07-17 22:41:59 +0530

    Time: 2 ms (94.66%) | Memory: 42.6 MB (90.76%) - LeetSync

--- 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.java
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.java
@@ -1,20 +1,15 @@
 class Solution {
     public boolean makeEqual(String[] arr) {
-        HashMap<Character, Integer> map = new HashMap<>();
+        int[] freq = new int[26];
         for(int i=0;i<arr.length;i++){
             String str = arr[i];
             for(int j=0;j<str.length();j++){
                 char ch = str.charAt(j);
-                if(map.containsKey(ch)){
-                    int freq = map.get(ch);
-                    map.put(ch, freq+1);
-                } else map.put(ch, 1);
+                freq[ch - 'a']++;
             }
         }
-
-        for(Character ch : map.keySet()){
-            int value = map.get(ch);
-            if(value % arr.length != 0) return false;
+        for(int ele : freq){
+            if(ele % arr.length != 0) return false;
         }
         return true;
     }
