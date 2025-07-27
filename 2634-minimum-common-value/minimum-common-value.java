d5b855b57d7e98cd96829cb1877524c951564e60
VivekKumar528 <122791268+VivekKumar528@users.noreply.github.com>
2025-07-27 11:38:14 +0530

    Time: 15 ms (15.26%) | Memory: 62.7 MB (47.70%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -1,14 +1,19 @@
 class Solution {
+    public int binarySearch(int target, int[] arr){
+        int low = 0;
+        int high = arr.length-1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(arr[mid] == target) return target;
+            else if(target < arr[mid]) high = mid-1;
+            else low = mid+1;
+        }
+        return Integer.MAX_VALUE;
+    }
     public int getCommon(int[] nums1, int[] nums2) {
-        HashSet<Integer> set1 = new HashSet<>();
-        HashSet<Integer> set2 = new HashSet<>();
-        for(int ele : nums1) set1.add(ele);
-        for(int ele : nums2) set2.add(ele);
         int ans = Integer.MAX_VALUE;
-        for(int ele : set1){
-            if(set2.contains(ele)){
-                if(ele < ans) ans = ele;
-            }
+        for(int i=0;i<nums1.length;i++){
+            ans =  Math.min(binarySearch(nums1[i], nums2), ans);
         }
         return ans == Integer.MAX_VALUE ? -1 : ans;
     }
