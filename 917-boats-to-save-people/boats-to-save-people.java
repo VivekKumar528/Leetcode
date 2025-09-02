7cd50047446961f2375f410a0a6452a316cd2fe5
VivekKumar528 <122791268+VivekKumar528@users.noreply.github.com>
2025-09-02 13:46:09 +0530

    Time: 16 ms (94.65%) | Memory: 55.1 MB (39.13%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.java
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -3,17 +3,16 @@ class Solution {
         Arrays.sort(arr);
         int len = arr.length;
         int i = 0;
-        int j = len-1;
+        int j = len - 1;
         int boat = 0;
-        while(i <= j){
-            if(arr[i] + arr[j] <= limit){
+        while (i <= j) {
+            if (arr[i] + arr[j] <= limit) {
                 i++;
                 j--;
-                boat++;
             } else {
                 j--;
-                boat++;
             }
+            boat++;
         }
         return boat;
     }
