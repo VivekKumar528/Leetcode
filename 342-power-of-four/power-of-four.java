8d79984b8961c0c9e26bb10b1859ad0f34abd96c
VivekKumar528 <122791268+VivekKumar528@users.noreply.github.com>
2025-09-09 08:43:47 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (61.71%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -1,10 +1,10 @@
 class Solution {
-    public boolean isPerfectSquare(int num){
-        int root = (int)(Math.sqrt(num));
-        return (root*root == num);
-    }
+    // public boolean isPerfectSquare(int num){
+    //     int root = (int)(Math.sqrt(num));
+    //     return (root*root == num);
+    // }
     public boolean isPowerOfFour(int n) {
         if(n <= 0) return false;
-        return ((n & (n-1)) == 0) && isPerfectSquare(n);
+        return ((n & (n-1)) == 0) && n%3 == 1;
     }
 }
