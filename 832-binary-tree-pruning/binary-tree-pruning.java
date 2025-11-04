c525fad0581e82e4c8fd3755645d6464b1329870
VivekKumar528 <122791268+VivekKumar528@users.noreply.github.com>
2025-11-04 23:38:38 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (6.31%) - LeetSync

--- 832-binary-tree-pruning/binary-tree-pruning.java
+++ 832-binary-tree-pruning/binary-tree-pruning.java
@@ -1,22 +1,12 @@
 class Solution {
-    public boolean isOnePresent(TreeNode node){
-        if(node == null) return false;
-
-        if(node.val == 1) return true;
-
-        return isOnePresent(node.left) || isOnePresent(node.right);
-    }
+    
     public TreeNode pruneTree(TreeNode root) {
-        if(root == null) return null;
+       if(root == null) return null;
+       root.left = pruneTree(root.left);
+       root.right = pruneTree(root.right);
 
-        if(!isOnePresent(root.left)) root.left = null;
-        if(!isOnePresent(root.right)) root.right = null;
+       if(root.left == null && root.right == null && root.val == 0) return null;
 
-        pruneTree(root.left);
-        pruneTree(root.right);
-
-        if(root.left == null && root.right == null && root.val == 0) return null;
-
-        return root;
+       return root;
     }
 }
