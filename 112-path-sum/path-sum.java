e5f78e1f064428bd420664b9fea2b3d30646d310
VivekKumar528 <122791268+VivekKumar528@users.noreply.github.com>
2025-11-23 21:09:08 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (27.25%) - LeetSync

---
+++ 112-path-sum/path-sum.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean helper(TreeNode root, int targetSum, int sum){
+        if(root == null) return false;
+        sum += root.val;
+        if(root.left == null && root.right == null){
+            if(sum == targetSum) return true;
+        }
+        return helper(root.left, targetSum, sum) || helper(root.right, targetSum, sum);
+    }
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        return helper(root, targetSum, 0);
+    }
+}
