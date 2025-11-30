87203517662c8a11a5efe4aded621287368a7019
VivekKumar528 <122791268+VivekKumar528@users.noreply.github.com>
2025-11-30 14:32:27 +0530

    Time: 9 ms (34.97%) | Memory: 50.3 MB (9.32%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,27 +1,43 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        int n = strs.length;
-        List<List<String>> ans = new LinkedList<>();
-        HashMap<String, List<String>> map = new HashMap<>();
-        for(int i=0;i<n;i++){
-            String temp = strs[i];
-            char[] ch = temp.toCharArray();
-            Arrays.sort(ch);
-            String key = new String(ch);
-            
-            if(map.containsKey(key)){
-                List<String> tempList = map.get(key);
-                tempList.add(strs[i]);
-                map.put(key,  tempList);
-            } else {
-                List<String> newList = new ArrayList<>();
-                newList.add(strs[i]);
-                map.put(key, newList);
+    public String generate(String str){
+        int[] arr = new int[26];
+        for(char ch : str.toCharArray()){
+            arr[ch-'a']++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<26;i++){
+            int freq = arr[i];
+            if(freq > 0){
+                sb.append((char)(i + 'a'));
+                sb.append(freq);
             }
         }
-        for(String k : map.keySet()){
-            ans.add(map.get(k));
+        return sb.toString();
+    }
+    public List<List<String>> groupAnagrams(String[] strs) {
+        int n = strs.length;
+
+        HashMap<String, List<String>> map = new HashMap<>();
+        List<List<String>> ans = new ArrayList<>();
+
+        for(int i=0;i<n;i++){
+            String word = strs[i];
+            String newWord = generate(word);
+            if(map.containsKey(newWord)){
+                List<String> list = map.get(newWord);
+                list.add(word);
+                map.put(newWord, list);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(word);
+                map.put(newWord, list);
+            }
         }
+
+        for(String key : map.keySet()){
+            ans.add(map.get(key));
+        }
+
         return ans;
     }
 }
