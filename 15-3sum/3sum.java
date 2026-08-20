3622d34e9e462ab7fa72f7ef5892617945fa6b8a
VivekKumar528 <122791268+VivekKumar528@users.noreply.github.com>
2026-08-21 01:38:34 +0530

    Time: 30 ms (87.92%) | Memory: 60.4 MB (7.39%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,32 +1,43 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] arr) {
-        Arrays.sort(arr);
-        List<List<Integer>> ans = new ArrayList<>();
-        if(arr.length < 3) return ans;
-        for (int x = 0; x < arr.length-2; x++) {
-            if(x > 0 && arr[x] == arr[x-1]) continue;
-            int target = -arr[x];
-            int i = x+1;
-            int j = arr.length - 1;
-            while (i < j) {
-                if (arr[i] + arr[j] > target)
-                    j--;
-                else if (arr[i] + arr[j] < target)
-                    i++;
-                else {
-                    while(i < j && arr[i] == arr[i+1]) i++;
-                    while(i < j && arr[j] == arr[j-1]) j--;
-                    List<Integer> temp = new ArrayList<>();
-                    temp.add(arr[x]);
-                    temp.add(arr[i]);
-                    temp.add(arr[j]);
-                    ans.add(temp);
-                    i++;
-                    j--;
-                }
-            }
+    List<List<Integer>> ans;
+    public void twoSum(int[] nums, int target, int i, int j){
+        while(i < j){
 
+            if(nums[i] + nums[j] > target) j--;
+            else if(nums[i] + nums[j] < target) i++;
+            else {
+                while(i < j && nums[i] == nums[i+1]) i++;
+                while(i < j && nums[j] == nums[j-1]) j--;
+                List<Integer> temp = new ArrayList<>();
+                temp.add(-target);
+                temp.add(nums[i]);
+                temp.add(nums[j]);
+
+                ans.add(temp);
+                i++;
+                j--;
+            }
         }
+
+    }
+    public List<List<Integer>> threeSum(int[] nums) {
+        ans = new ArrayList<>();
+        int len = nums.length;
+
+        if(len < 3) return new ArrayList<List<Integer>>();
+        // ans.clear();
+        Arrays.sort(nums);
+
+        for(int i=0;i<len;i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            int n1 = nums[i];
+            int target = -n1;
+
+            twoSum(nums, target, i+1, len-1);
+        }
+
         return ans;
+
+
     }
 }
