2a3b5ff14f037de6097a2fc775945b36e0229249
VivekKumar528 <122791268+VivekKumar528@users.noreply.github.com>
2026-08-22 02:36:15 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (44.08%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,41 +1,60 @@
 class Solution {
-    public List<Integer> spiralOrder(int[][] arr) {
-        int m = arr.length;
-        int n = arr[0].length;
-        List<Integer> ans = new LinkedList<>();
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
         int dir = 0;
-        int left = 0;
-        int right = n-1;
         int top = 0;
         int down = m-1;
-        while(ans.size() < (m*n)) {
+        int left = 0;
+        int right = n-1;
+        /*
+            dir = 0 => left to right
+            dir = 1 => top to down
+            dir = 2 => right to left
+            dir = 3 => down to top
+        */
+
+        List<Integer> result = new ArrayList<>();
+
+        while(top <= down && left <= right){
             if(dir == 0){
-                for(int j=left; j<=right; j++){
-                    ans.add(arr[top][j]);
+                // left to right
+                // constant : row (top)
+                for(int j=left; j<=right;j++){
+                    result.add(matrix[top][j]);
                 }
                 top++;
             }
+
             if(dir == 1){
+                // up to down
+                // constant : col (right)
                 for(int i=top;i<=down;i++){
-                    ans.add(arr[i][right]);
+                    result.add(matrix[i][right]);
                 }
                 right--;
             }
-            if(dir == 2){ // right to left
+
+            if(dir == 2){
+                // right to left
+                // constant : row (down)
                 for(int j=right;j>=left;j--){
-                    ans.add(arr[down][j]);
+                    result.add(matrix[down][j]);
                 }
                 down--;
             }
-            if(dir == 3){ // down to top
+            if(dir == 3){
+                // down to top
+                // constant : col (left)
                 for(int i=down;i>=top;i--){
-                    ans.add(arr[i][left]);
+                    result.add(matrix[i][left]);
                 }
                 left++;
             }
             dir++;
             if(dir == 4) dir = 0;
         }
-        return ans;
+        return result;
     }
 }
