5c859ace7c66f6a4dd5a6bba671b69da20542a7e
VivekKumar528 <122791268+VivekKumar528@users.noreply.github.com>
2026-08-22 20:00:57 +0530

    Time: 21 ms (5.21%) | Memory: 46.9 MB (18.38%) - LeetSync

--- 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.java
+++ 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.java
@@ -1,28 +1,30 @@
 class Solution {
-    public int[][] diagonalSort(int[][] arr) {
-        int m =arr.length;
-        int n = arr[0].length;
+    public int[][] diagonalSort(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
         HashMap<Integer, List<Integer>> map = new HashMap<>();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                int key = i-j;
+                int key = i - j;
                 if(map.containsKey(key)){
-                    map.get(key).add(arr[i][j]);
+                    List<Integer> val = map.get(key);
+                    val.add(mat[i][j]);
+                    map.put(key, val);
                 } else {
-                    List<Integer> list = new ArrayList<>();
-                    list.add(arr[i][j]);
-                    map.put(key, list);
+                    ArrayList<Integer> newList = new ArrayList<>();
+                    newList.add(mat[i][j]);
+                    map.put(i - j, newList);
                 }
             }
         }
-        for(int key : map.keySet()){
-            Collections.sort(map.get(key));
-        }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                arr[i][j] = map.get(i-j).remove(0);
+                List<Integer> key = map.get(i - j);
+                Collections.sort(key);
+                mat[i][j] = key.get(0);
+                key.remove(0);
             }
         }
-        return arr;
+        return mat;
     }
 }
