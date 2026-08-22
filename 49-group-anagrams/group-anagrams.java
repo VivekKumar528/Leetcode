17e27a0d229f5d6f75db46c2fd8e23706ffa7cba
VivekKumar528 <122791268+VivekKumar528@users.noreply.github.com>
2026-08-22 11:48:07 +0530

    Time: 7 ms (72.55%) | Memory: 49.8 MB (32.67%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,43 +1,27 @@
 class Solution {
-    public String generate(String str){
-        int[] arr = new int[26];
-        for(char ch : str.toCharArray()){
-            arr[ch-'a']++;
-        }
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<26;i++){
-            int freq = arr[i];
-            if(freq > 0){
-                sb.append((char)(i + 'a'));
-                sb.append(freq);
-            }
-        }
-        return sb.toString();
-    }
     public List<List<String>> groupAnagrams(String[] strs) {
-        int n = strs.length;
-
+        int len = strs.length;
         HashMap<String, List<String>> map = new HashMap<>();
-        List<List<String>> ans = new ArrayList<>();
-
-        for(int i=0;i<n;i++){
-            String word = strs[i];
-            String newWord = generate(word);
-            if(map.containsKey(newWord)){
-                List<String> list = map.get(newWord);
-                list.add(word);
-                map.put(newWord, list);
+        for(int i=0;i<len;i++){
+            String str = strs[i];
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sorted = new String(chars);
+            if(map.containsKey(sorted)){
+                List<String> val = map.get(sorted);
+                val.add(str);
             } else {
-                List<String> list = new ArrayList<>();
-                list.add(word);
-                map.put(newWord, list);
+                List<String> temp = new ArrayList<>();
+                temp.add(str);
+                map.put(sorted, temp);
             }
         }
-
+        List<List<String>> res = new ArrayList<>();
         for(String key : map.keySet()){
-            ans.add(map.get(key));
+            List<String> val = map.get(key);
+            res.add(val);
         }
+        return res;
 
-        return ans;
     }
 }
