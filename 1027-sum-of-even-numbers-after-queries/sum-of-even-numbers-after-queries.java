5fd824fe976553d005b1872364ef7d9a6f9fa923
VivekKumar528 <122791268+VivekKumar528@users.noreply.github.com>
2026-08-23 11:46:07 +0530

    Time: 2353 ms (18.77%) | Memory: 52.5 MB (89.79%) - LeetSync

--- 1027-sum-of-even-numbers-after-queries/sum-of-even-numbers-after-queries.java
+++ 1027-sum-of-even-numbers-after-queries/sum-of-even-numbers-after-queries.java
@@ -1,16 +1,18 @@
 class Solution {
-    public int[] sumEvenAfterQueries(int[] arr, int[][] q) {
-        int len = arr.length;
-        int[] ans = new int[q.length];
-        for(int i=0;i<q.length;i++){
-            int idx = q[i][1];
-            arr[idx] += q[i][0];
-            int sum = 0;
-            for(int j=0;j<len;j++){
-                if(arr[j] % 2 == 0) sum+= arr[j];
+    public int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
+        int len = queries.length;
+        int idx = 0;
+        int[] res = new int[len];
+        for(int i=0;i<len;i++){
+            int qVal = queries[i][0];
+            int qIdx = queries[i][1];
+            nums[qIdx] += qVal;
+            int evenSum = 0;
+            for(int ele : nums){
+                if(ele % 2 == 0) evenSum += ele;
             }
-            ans[i] = sum;
+            res[idx++] = evenSum;
         }
-        return ans;
+        return res;
     }
 }
