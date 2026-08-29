681905b17ad3da6b33c0fe4e48ae8b6c570f8a18
VivekKumar528 <122791268+VivekKumar528@users.noreply.github.com>
2026-08-29 11:28:34 +0530

    Time: 14 ms (6.00%) | Memory: 52.3 MB (15.90%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,50 +1,20 @@
 class Solution {
-    public boolean validBox(char[][] board, int sr, int er, int sc, int ec){
-        HashSet<Character> set = new HashSet<>();
-        for(int i=sr;i<=er;i++){
-            for(int j=sc;j<=ec;j++){
-                char ch = board[i][j];
-                if(ch == '.') continue;
-                if(set.contains(ch)) return false;
-                set.add(ch);
-
-            }
-        }
-        return true;
-    }
     public boolean isValidSudoku(char[][] board) {
-        int m = board.length;
-        int n = m;
+        Set<String> set = new HashSet<>();
         for(int i=0;i<9;i++){
-            HashSet<Character> set = new HashSet<>();
             for(int j=0;j<9;j++){
-                char ch = board[i][j];
-                if(ch == '.') continue;
-                if(set.contains(ch)) return false;
-                set.add(ch);
+                if(board[i][j] == '.') continue;
+
+                String row = board[i][j] + "_ROW_" + i;
+                String col = board[i][j] + "_COL_" + j;
+                String box = board[i][j] + "_BOX_" + (i/3) + (j/3);
+
+                if(set.contains(row) || set.contains(col) || set.contains(box)) return false;
+                set.add(row);
+                set.add(col);
+                set.add(box);
             }
         }
-
-        for(int j=0;j<9;j++){
-            HashSet<Character> set = new HashSet<>();
-            for(int i=0;i<9;i++){
-                char ch = board[i][j];
-                if(ch == '.') continue;
-                if(set.contains(ch)) return false;
-                set.add(ch);
-            }
-        }
-
-        for(int sr=0;sr<9;sr+=3){
-            int er = sr + 2;
-            
-            for(int sc=0;sc<9;sc+=3){
-                int ec = sc + 2;
-                if(!validBox(board, sr, er, sc, ec)) return false;
-            }
-        }
-
-
         return true;
     }
 }
