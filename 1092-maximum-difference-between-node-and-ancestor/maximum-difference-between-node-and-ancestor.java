3a684aad3f13a19ab30c21df40d2583638af96c8
VivekKumar528 <122791268+VivekKumar528@users.noreply.github.com>
2026-09-08 14:29:15 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (57.01%) - LeetSync

--- 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.java
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.java
@@ -2,10 +2,8 @@ class Solution {
     int diff = 0;
 
     void traversal(TreeNode root, int min, int max) {
-        if (root == null)
-            return;
+        if (root == null) return;
         diff = Math.max(diff, Math.max(Math.abs(root.val - min), Math.abs(root.val - max)));
-
         traversal(root.left, Math.min(root.val, min), Math.max(root.val, max));
         traversal(root.right, Math.min(root.val, min), Math.max (root.val, max));
     }
