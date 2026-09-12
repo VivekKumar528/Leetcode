98907696efc99ab0a91dc839f26e118048b54370
VivekKumar528 <122791268+VivekKumar528@users.noreply.github.com>
2026-09-12 22:53:08 +0530

    Time: 11 ms (15.81%) | Memory: 43.5 MB (8.37%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,29 +1,30 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        // HashMap<Character, Integer> map = new HashMap<>();
-        // for(char ch : s.toCharArray()){
-        //     if( map.containsKey(ch)){
-        //         int val = map.get(ch);
-        //         map.put(ch, val+1);
-        //     } else map.put(ch, 1);
-        // }
-        // for(char ch : t.toCharArray()){
-        //     if(!map.containsKey(ch)) return ch;
-        //     else {
-        //         int val = map.get(ch);
-        //         map.put(ch, val-1);
-        //     }
-        // }
-        // return 'a';
-
-        int sAsciiSum = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
         for(char ch : s.toCharArray()){
-            sAsciiSum += (int)ch;
+            if( map.containsKey(ch)){
+                int val = map.get(ch);
+                map.put(ch, val+1);
+            } else map.put(ch, 1);
         }
-        int tAsciiSum = 0;
         for(char ch : t.toCharArray()){
-            tAsciiSum += (int)ch;
+            if(!map.containsKey(ch)) return ch;
+            else {
+                int val = map.get(ch);
+                if(val == 0) return ch;
+                map.put(ch, val-1);
+            }
         }
-        return  (char)Math.abs(sAsciiSum - tAsciiSum);
+        return 'a';
+
+        // int sAsciiSum = 0;
+        // for(char ch : s.toCharArray()){
+        //     sAsciiSum += (int)ch;
+        // }
+        // int tAsciiSum = 0;
+        // for(char ch : t.toCharArray()){
+        //     tAsciiSum += (int)ch;
+        // }
+        // return  (char)Math.abs(sAsciiSum - tAsciiSum);
     }
 }
