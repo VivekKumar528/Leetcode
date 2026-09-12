f3bf3ef3e902a93c733d0ec6917bda6b5aff4e24
VivekKumar528 <122791268+VivekKumar528@users.noreply.github.com>
2026-09-13 02:16:46 +0530

    Time: 2 ms (97.27%) | Memory: 46.8 MB (96.56%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
@@ -1,24 +1,23 @@
 class Solution {
-    boolean check(int[][] arr, int row, int col){
-        int count = 0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i][col] == 1) count++;
-            if(count >= 2) return false;
-        }
-        count = 0;
-        for(int j=0;j<arr[0].length;j++){
-            if(arr[row][j] == 1) count++;
-            if(count >= 2) return false;
-        }
-        return true;
-    }
     public int numSpecial(int[][] arr) {
         int m = arr.length;
         int n = arr[0].length;
+
+        int[] rowTrack = new int[m];
+        int[] colTrack = new int[n];
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(arr[i][j] == 1) {
+                    rowTrack[i] += 1;
+                    colTrack[j] += 1;
+                }
+            }
+        }
         int res = 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(arr[i][j] == 1 && check(arr, i, j)) res++;
+                if(arr[i][j] == 1 && rowTrack[i] == 1 && colTrack[j] == 1) res += 1;
             }
         }
 
