806ff4a3a759c82f5e9c79d3ef7f2411f50b1a7c
VivekKumar528 <122791268+VivekKumar528@users.noreply.github.com>
2026-09-14 11:36:23 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (49.30%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -1,21 +1,12 @@
 class Solution {
-    public void solve(TreeNode root, List<StringBuilder> list, StringBuilder str){
-        if(root == null) return;
-        if(root.left == null && root.right == null){
-            list.add(new StringBuilder(str).append(root.val));
-            return;
-        }
-        solve(root.left, list, new StringBuilder(str).append(root.val));
-        solve(root.right, list, new StringBuilder(str).append(root.val));
+    public int solve(TreeNode root, int currNum){
+        if(root == null) return 0;
+        currNum = (currNum * 10)+ root.val;
+        if(root.left == null &&  root.right == null) return currNum;
+        return solve(root.left, currNum) + solve(root.right, currNum);
+
     }
     public int sumNumbers(TreeNode root) {
-      List<StringBuilder> list = new LinkedList<>();
-      solve(root, list, new StringBuilder());
-      long sum = 0;
-      for(StringBuilder str : list){
-        long num = Integer.parseInt(str.toString());
-        sum += num;
-      }
-      return (int)sum;
+        return solve(root, 0);
     }
 }
