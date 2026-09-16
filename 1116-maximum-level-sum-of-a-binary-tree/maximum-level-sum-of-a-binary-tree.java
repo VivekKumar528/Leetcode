d333f8736747c08df30c5489edfe2e070398d701
VivekKumar528 <122791268+VivekKumar528@users.noreply.github.com>
2026-09-16 15:20:08 +0530

    Time: 10 ms (16.71%) | Memory: 48.7 MB (95.73%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.java
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.java
@@ -1,31 +1,30 @@
 class Solution {
+    public void inOrder(TreeNode root, int level, HashMap<Integer, Integer> map){
+        if(root == null) return;
+        inOrder(root.left, level+1, map);
+        if(map.containsKey(level)){
+            int levelSum = map.get(level);
+            levelSum += root.val;
+            map.put(level, levelSum);
+        } else map.put(level, root.val);
+        inOrder(root.right, level+1, map);
+
+
+
+    }  
     public int maxLevelSum(TreeNode root) {
+        HashMap<Integer, Integer> map = new HashMap<>(); // level, sum
+        inOrder(root, 1, map);
+
         int maxSum = Integer.MIN_VALUE;
-        int resultLevel = 0;
-
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-
-        int currLevel = 1;
-
-        while(!q.isEmpty()){
-            int n = q.size();
-            int sum = 0;
-            while(n --> 0){
-                TreeNode temp = q.poll();
-                sum += temp.val;
-                if(temp.left != null) q.offer(temp.left);
-                if(temp.right != null) q.offer(temp.right);
+        int resLevel = 1;
+        for(int level : map.keySet()){
+            int levelSum = map.get(level);
+            if(levelSum > maxSum){
+                maxSum = levelSum;
+                resLevel = level; 
             }
-
-            if(sum > maxSum){
-                maxSum = sum;
-                resultLevel = currLevel;
-            }
-            currLevel++;
         }
-
-        return resultLevel;
-
+        return resLevel;
     }
 }
