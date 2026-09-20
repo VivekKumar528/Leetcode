f7dab9d714e91b8f1f895bd8a2bcb5f49dd54cf3
VivekKumar528 <122791268+VivekKumar528@users.noreply.github.com>
2026-09-20 15:54:59 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (87.08%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,24 +1,39 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
+
+    public int leftBinarySearch(int[] arr, int target){
+        int l = 0;
+        int r = arr.length - 1;
+        int leftMostIdx = -1;
+        while(l <= r){
+            int mid = l + (r-l)/2;
+            if(arr[mid] == target){
+                leftMostIdx = mid;
+                r = mid - 1;
+            } else if(arr[mid] > target) r = mid-1;
+            else l = mid + 1;
+        }
+        return leftMostIdx;
+    }
+
+    public int rightBinarySearch(int[] arr, int target){
+        int l = 0;
+        int r = arr.length - 1;
+        int rightMostIdx = -1;
+        while(l <= r){
+            int mid = l + (r-l)/2;
+            if(arr[mid] == target){
+                rightMostIdx = mid;
+                l = mid+1;
+            } else if(arr[mid] > target) r = mid-1;
+            else l = mid+1;
+        }
+        return rightMostIdx;
+    }
+    public int[] searchRange(int[] arr, int target) {
         int[] ans = new int[2];
-        int leftIdx = -1;
-        int rightIdx = -1;
-        int left = 0;
-        int right = nums.length-1;
-        while(left <= right){
-            int mid = left + (right-left)/2;
-            if(nums[mid] >= target) right = mid -1;
-            else left = mid+1;
-            if(nums[mid] == target) leftIdx = mid;
-        }
-        left = 0; right = nums.length-1;
-        while(left <= right){
-            int mid = left + (right-left)/2;
-            if(nums[mid] <= target) left = mid+1;
-            else right = mid-1;
-            if(nums[mid] == target) rightIdx = mid;
-        }
-        ans[0] = leftIdx; ans[1] = rightIdx;
+        ans[0] = leftBinarySearch(arr, target);
+        ans[1] = rightBinarySearch(arr, target);
+
         return ans;
     }
 }
